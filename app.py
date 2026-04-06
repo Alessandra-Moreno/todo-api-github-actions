b2f039fcd6a00e5ac0ca21271d9c537a087c10f9
Alessandra Moreno <alessandramoreno543@gmail.com>
2026-04-06 08:28:09 -0300

    tarefas clicaveis com toggle de status

--- app.py
+++ app.py
@@ -1,48 +1,36 @@
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 tarefas = []
+contador_id = 1
 
+@app.route('/')
+def index():
+    return render_template('index.html', tarefas=tarefas)
 
-@app.route("/")
-def home():
-    return render_template("index.html", tarefas=tarefas)
+@app.route('/add', methods=['POST'])
+def add():
+    global contador_id
+    nome = request.form.get('nome')
 
+    if nome:
+        tarefas.append({
+            "id": contador_id,
+            "nome": nome,
+            "feito": False
+        })
+        contador_id += 1
 
-@app.route("/tarefas", methods=["GET"])
-def listar():
-    return jsonify(tarefas)
+    return redirect('/')
 
+@app.route('/toggle/<int:id>', methods=['POST'])
+def toggle(id):
+    for tarefa in tarefas:
+        if tarefa["id"] == id:
+            tarefa["feito"] = not tarefa["feito"]
+            break
+    return redirect('/')
 
-@app.route("/tarefas", methods=["POST"])
-def adicionar():
-    if request.is_json:
-        data = request.json
-        titulo = data.get("titulo")
-    else:
-        titulo = request.form.get("titulo")
-
-    tarefa = {
-        "id": len(tarefas) + 1,
-        "titulo": titulo,
-        "concluida": False
-    }
-
-    tarefas.append(tarefa)
-
-    return redirect("/")  # volta pra página principal
-
-
-@app.route("/tarefas/<int:id>", methods=["PUT"])
-def concluir(id):
-    for t in tarefas:
-        if t["id"] == id:
-            t["concluida"] = True
-            return jsonify(t)
-
-    return {"erro": "Tarefa não encontrada"}, 404
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
